2518ab5aacef516d26abd7b483c5eb8c22c2d2d5
Joe Klein <jklein694@gmail.com>
2017-11-08 15:04:31 -0500

    skeleton but model needs improving

---
+++ check.py
@@ -0,0 +1,67 @@
+import tensorflow as tf
+import os
+import numpy as np
+import cv2
+
+
+
+def input_parser(filenames):
+    # convert the label to one-hot encoding
+    # step 2
+    images = []
+    image_size = 28
+    image = cv2.imread(filenames, cv2.IMREAD_GRAYSCALE)
+
+    # Resizing the image to our desired size and
+    # preprocessing will be done exactly as done during training
+    image = cv2.resize(image, (image_size, image_size))
+    images.append(image)
+    images = np.array(images, dtype=np.uint8)
+    images = images.astype('float32')
+
+    images = images.ravel()
+    X_check = images.reshape(-1, 784)
+
+    return X_check
+
+def restore_model(X_check):
+    model_path = "saved_model/model.ckpt"
+
+    y_correct = np.array([0.00000000, 1.000000000]).reshape(-1, 2)
+
+
+
+    with tf.Session() as sess:
+        saver = tf.train.import_meta_graph(model_path + '.meta')
+        saver.restore(sess, model_path)
+        # access a variable from the saved Graph, and so on:
+
+        graph = tf.get_default_graph()
+
+        x = graph.get_tensor_by_name('x_placeholder:0')
+        y = graph.get_tensor_by_name('y_placeholder:0')
+
+        correct_prediction = graph.get_tensor_by_name('correct_prediction:0')
+
+
+        y_hat = sess.run(correct_prediction, feed_dict={x: X_check, y:y_correct})
+
+    return y_hat
+
+
+def main():
+    file_path = 'not_hotdog.jpg'
+    image_check = input_parser(file_path)
+    prediction = restore_model(image_check)
+    if prediction[0] == True:
+        guess = 'Hotdog'
+        print('This is a ', guess, '!')
+    else:
+        guess = 'not a hotdog'
+        print('This is ', guess)
+
+
+
+
+if __name__ == "__main__":
+    main()
--- restore.py
+++ restore.py
@@ -1,15 +1,30 @@
 import tensorflow as tf
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
+model_path = "saved_model/model.ckpt"
 
+X_ = pd.read_csv('X.csv').values
+Y_ = pd.read_csv('Y.csv').values
 
+X_train, X_test, y_train, y_test = train_test_split(X_, Y_, test_size=0.2, random_state=42)
 
 init = tf.global_variables_initializer()
-saver = tf.train.Saver()
 
-    with tf.Session() as sess:
-        # Initialize variables
-        sess.run(init)
 
-        # Restore model weights from previously saved model
-        saver.restore(sess, model_path)
-        print("Model restored from file: %s" % save_path)
+with tf.Session() as sess:
+    saver = tf.train.import_meta_graph(model_path + '.meta')
+    saver.restore(sess, model_path)
+    # access a variable from the saved Graph, and so on:
+
+    graph = tf.get_default_graph()
+    cost = graph.get_tensor_by_name("loss:0")
+    accuracy = graph.get_tensor_by_name('accuracy:0')
+    x = graph.get_tensor_by_name('x_placeholder:0')
+    y = graph.get_tensor_by_name('y_placeholder:0')
+    output = graph.get_tensor_by_name('output:0')
+    correct_prediction = graph.get_tensor_by_name('correct_prediction:0')
+
+    cp, a = sess.run([correct_prediction, accuracy], feed_dict={x: X_test, y: y_test})
+    print(cp)
+    print(a)
