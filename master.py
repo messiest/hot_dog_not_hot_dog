57281270cde3ce497c5976732e99bad20dd91ba5
Joe Klein <jklein694@gmail.com>
2017-11-13 17:03:19 -0500

    Master decommented out

--- master.py
+++ master.py
@@ -1,7 +1,10 @@
 import time
 
 import check
+import convolutional_nn as cnn
+import get_raw_images as scrape_images
 import image_augmentation as ia
+import remove_null_images as rm
 import restore_to_test as restore_test
 
 start_time = time.time()
@@ -13,16 +16,16 @@ model_path = "conv_model/model.ckpt"
 
 def main():
     # This function will scrape images from Image.net
-    # scrape_images.read_images_to_folder()
+    scrape_images.read_images_to_folder()
 
     # Removing all of the invalid or null images
-    # rm.remove_invalid(paths)
+    rm.remove_invalid(paths)
 
     # Rotating and or blurring images to create larger and balanced classes
     x, y = ia.load_data(128, 15000)
 
     # Run convolutional neural network
-    # total_loss, accuracy = cnn.run(x, y, epochs=10, learning_rate=0.001)
+    total_loss, accuracy = cnn.run(x, y, epochs=10, learning_rate=0.001)
 
     # Restore convolutional neural network and build confusion matrix
     restore_test.run_test(x, y, model_path)
